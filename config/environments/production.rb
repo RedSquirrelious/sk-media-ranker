4ccf8a8ce239e87b5749909d6eb22eeaf1f6b949
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-16 20:30:59 -0700

    following classmate-instructor advice on how to fix missing images on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,9 +26,9 @@ Rails.application.configure do
 
   config.serve_static_files = true
 
-  # config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
+  config.serve_static_assets = true
+
+
 
 
   # Compress JavaScripts and CSS.
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
